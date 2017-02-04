bcdab11a46badd2ca04404c1d3ad0de0245d637d
miamiworldwide <rivendalejones@gmail.com>
2017-02-04 15:52:17 -0800

    adding video overlay

--- sketch.js
+++ sketch.js
@@ -1,32 +1,50 @@
 //var pepeSmile = image("pepeSmile.png")
+var playing = false;
+var resist;
+var pepeSmile;
+var blueButton;
+var redButton;
 
 
 function setup() {
-  // create canvas
-  var c = createCanvas(710, 400);
-  background(100);
-  // Add an event for when a file is dropped onto the canvas
-  c.drop(gotFile);
+  createCanvas(450, 400);
+  // specify multiple formats for different browsers
+  pepeSmile = createVideo(['images/frown.mov']);
+  resist = createVideo(['images/womensmarchgif2.mov']);
+
+//  pepeSmile.loop(); // set the video to loopv and start playing
+  pepeSmile.hide(); // by default video shows up in separate dom
+                  // element. hide it and draw it to the canvas
+                  // insteads
+ resist.hide();
+ blueButton = createButton('blue');
+ redButton = createButton('red');
+ blueButton.mousePressed(toggleVid); // attach button listener
+ redButton.mousePressed(toggleVid); // attach button listener
+
 }
+
 
 function draw() {
-  fill(255);
-  noStroke();
-  textSize(24);
-  textAlign(CENTER);
-  text('Drag an image file onto the canvas.', width/2, height/2);
-  noLoop();
-  image("pepeSmile", width/2, height/2)
+//  background(255);
+
+//  image(pepeSmile,10,10); // draw the video frame to canvas
+//  filter('GRAY');
+  tint(255, 100);  // Display at half opacity
+  image(resist, 0,0, 700,400);
+  image(pepeSmile,0,0);
+
 }
 
-function gotFile(file) {
-  // If it's an image file
-  if (file.type === 'image') {
-    // Create an image DOM element but don't show it
-    var img = createImg(file.data).hide();
-    // Draw the image onto the canvas
-    image(img, 0, 0, width, height);
+function toggleVid() {
+  if (playing) {
+    pepeSmile.pause();
+    resist.pause();
+
   } else {
-    println('Not an image file!');
+    pepeSmile.play();
+    resist.loop();
+
   }
+  playing = !playing;
 }
